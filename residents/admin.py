f654094ebfd41a16b2a002958d40f40dc444b9c5
KostyaArent <trepalin.konstantin@gmail.com>
2022-04-12 23:08:23 +0000

    primary models added

--- residents/admin.py
+++ residents/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (Region, Location, House, Own, IndividualResident,
+    EntityResident, Organization, BaseResidentRel,)
 
 # Register your models here.
+admin.site.register(Region)
+admin.site.register(Location)
+admin.site.register(House)
+admin.site.register(Own)
+admin.site.register(IndividualResident)
+admin.site.register(EntityResident)
+admin.site.register(Organization)
+admin.site.register(BaseResidentRel)
--- residents/models.py
+++ residents/models.py
@@ -1,3 +1,141 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class BaseLocation(models.Model):
+    """Абстрактная базовая модель локации.
+    :param title: название
+    :type title: CharField
+    """
+    title = models.CharField(max_length=20, unique=True)
+
+
+class Region(BaseLocation):
+    code = models.IntegerField(unique=True, validators=[MaxValueValidator(999)])
+
+
+class Location(BaseLocation):
+    TYPES = (
+    ('district', 'Район'),
+    ('town', 'Город'),
+    ('street', 'Улица'),
+    )
+    type = models.CharField(max_length=300, choices = TYPES)
+    def __str__(self):
+        return self.title
+
+
+class House(models.Model):
+    """Модель Здания.
+    :param number: Номер здания
+    :type number: CharField
+    :param region: Экземпляр класса Region
+    :type region: ForeignKey
+    :param district: Экземпляр класса District
+    :type district: ForeignKey
+    :param town: Экземпляр класса Town
+    :type town: ForeignKey
+    :param street: Экземпляр класса Street
+    :type street: ForeignKey
+    """
+    number = models.CharField(max_length=20, unique=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    district = models.ForeignKey(Location, related_name='Район', on_delete=models.CASCADE, limit_choices_to={'type': 'district'})
+    town = models.ForeignKey(Location, related_name='Город', on_delete=models.CASCADE, limit_choices_to={'type': 'town'})
+    street = models.ForeignKey(Location, related_name='Улица', on_delete=models.CASCADE, limit_choices_to={'type': 'street'})
+
+
+
+class BaseResident(models.Model):
+    """Абстрактная базовая модель собственника.
+    :param itn: ИНН
+    :type itn: CharField
+    """
+    itn = models.CharField(default=0, max_length=20)
+
+    class Meta:
+        abstract = True
+
+
+
+class IndividualResident(BaseResident):
+    """Модель собственника физического лица.
+    :param user: Экземпляр класса User
+    :type user: ForeignKey
+    :param own: Экземпляры класса Own
+    :type own: ManyToManyField
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+class EntityResident(BaseResident):
+    """Модель собственника юридического лица.
+    :param title: краткое наименование юр.лица
+    :type title: CharField
+    :param full_title: полное наименование юр.лица
+    :type full_title: CharField
+    :param kpp: КПП
+    :type kpp: CharField
+    :param staff: Экземпляры класса User
+    :type staff: ManyToManyField
+    """
+    title = models.CharField(default=0, max_length=50)
+    full_title = models.CharField(default=0, max_length=100)
+    kpp = models.CharField(default=0, max_length=20)
+    staff = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class BaseResidentRel(models.Model):
+    entity_resident = models.ForeignKey(EntityResident, models.SET_NULL, blank=True, null=True, related_name='mtm')
+    individual_resident = models.ForeignKey(IndividualResident, models.SET_NULL, blank=True, null=True, related_name='mtm')
+
+    def __str__(self):
+        if self.entity_resident is not None:
+            return self.entity_resident.title
+        elif self.individual_resident is not None:
+            return self.individual_resident.user.username
+
+
+class Own(models.Model):
+    """Модель Помещения собственности.
+    :param number: Номер помещения
+    :type number: CharField
+    :param house: Экземпляр класса House
+    :type house: ForeignKey
+    :param owners: Экземпляры класса BaseResident
+    :type owners: ManyToManyField
+    :param is_living: статус жилого помещения
+    :type is_living: BooleanField
+    """
+    number = models.CharField(max_length=20, unique=True)
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
+    owners = models.ManyToManyField(BaseResidentRel)
+    is_living = models.BooleanField()
+
+
+class Organization(BaseResident):
+    """Модель обслуживающей организации.
+    :param title: краткое наименование юр.лица
+    :type title: CharField
+    :param full_title: полное наименование юр.лица
+    :type full_title: CharField
+    :param kpp: КПП
+    :type kpp: CharField
+    :param staff: Экземпляры класса User
+    :type staff: ManyToManyField
+    """
+    title = models.CharField(default=0, max_length=50)
+    full_title = models.CharField(default=0, max_length=100)
+    kpp = models.CharField(default=0, max_length=20)
+    staff = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.title
