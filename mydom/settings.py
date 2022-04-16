5e4896470c47f1bd3e2562d41abb1bdb0311b06c
KostyaArent <trepalin.konstantin@gmail.com>
2022-04-16 18:27:05 +0000

    flake8 added to project

--- mydom/settings.py
+++ mydom/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4a()c-fy!nt94xuryhq=^5#4g--3$pu(vu%3*$56)vmo&_=j$*'
+SECRET_KEY = 'django-insecure-4a()c-f!nt94xuryhq=^5#4g--3$u(vu%3*$56)vmo&_=j$*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,16 +87,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+        NumericPasswordValidator',
     },
 ]
 
--- mydom/urls.py
+++ mydom/urls.py
@@ -1,30 +1,14 @@
-"""mydom URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from residents.views import signin
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-        # AUTH
-    #path('signup/', views.signupuser, name='signupuser'),
+    # AUTH
     path('signin/', signin, name='signin'),
-    #path('logout/', views.logoutuser, name='logoutuser'),
 
-        # RESIDENTS APP
+    # RESIDENTS APP
     path('residents/', include('residents.urls'), name='residents'),
 ]
